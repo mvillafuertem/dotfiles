95dba2522c2720a1074114b5416697bd9e737621
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2024-05-10 12:07:50 +0200

    enhancements

--- modules/home-manager/k9s/default.nix
+++ modules/home-manager/k9s/default.nix
@@ -1,6 +1,14 @@
 { config, ... }: {
-  xdg.configFile.k9s = {
-    source = config.lib.file.mkOutOfStoreSymlink ./config;
+  xdg.configFile = {
+    "k9s" = { source = config.lib.file.mkOutOfStoreSymlink ./config; };
+    # "k9s/skins/catppuccin-mocha.yaml" = {
+    #   source = (pkgs.fetchFromGitHub {
+    #     owner = "catppuccin";
+    #     repo = "k9s";
+    #     rev = "82eba6feb442932e28facedfb18dfbe79234f180";
+    #     sha256 = "VLi7G6Rjmbr6feSOg8aLYJmOb+GyJUKi3k9qod6ut9k=";
+    #   } + "/dist/catppuccin-mocha.yaml");
+    # };
   };
   programs.k9s = { enable = true; };
 }
