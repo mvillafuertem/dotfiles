0e2544c7c49d9bd827d5981e8a1fe37c4fbfa34f
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2025-02-26 19:24:37 +0100

    add lazy-lock

--- modules/home-manager/nvim/config/init.lua
+++ modules/home-manager/nvim/config/init.lua
@@ -1,28 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("mvillafuerte")
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-	},
-	default = {
-		lazy = true,
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
+  spec = {
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+  },
+  lockfile = vim.fn.expand("$HOME/.dotfiles/modules/home-manager/nvim/config/lazy-lock.json"),
+  checker = {
+    enabled = true,
+    notify = false,
+  },
   ui = {
     border = "rounded",
   },
