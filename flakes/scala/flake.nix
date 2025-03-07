b266ea59a9198eaae46ccb241c8eff9971c7b62f
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2025-03-07 23:30:06 +0100

    enhance scala flake

--- flakes/scala/flake.nix
+++ flakes/scala/flake.nix
@@ -1,28 +1,35 @@
 {
   description = "Flake for Scala Development";
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "aarch64-darwin";
-      java = "openjdk11";
-      jdk = pkgs.${java};
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            # sbt = prev.sbt.overrideAttrs { postPatch = ''
-            sbt = prev.sbt.overrideAttrs {
-              patchPhase = ''
-                echo -java-home ${jdk} >> conf/sbtopts
-              '';
-            };
-          })
-        ];
-      };
-    in {
+  inputs.systems.url = "github:nix-systems/default";
 
-      devShells.${system}.default =
-        pkgs.mkShell { buildInputs = [ jdk pkgs.sbt ]; };
-    };
+  outputs = { systems, nixpkgs, ... }: {
+    devShells = nixpkgs.lib.genAttrs (import systems) (system:
+      let
+        jdk = pkgs.openjdk8;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              sbt = prev.sbt.overrideAttrs {
+                patchPhase = ''
+                  echo -java-home ${jdk} >> conf/sbtopts
+                '';
+              };
+            })
+          ];
+        };
+      in {
+        default = pkgs.mkShell {
+          name = "scala";
+          buildInputs = [ jdk pkgs.sbt pkgs.metals pkgs.figlet ];
+          shellHook = ''
+            [ ! -f /tmp/figlet/Shadow.flf ] &&\
+            mkdir -p /tmp/figlet &&\
+            curl -L https://raw.githubusercontent.com/xero/figlet-fonts/master/ANSI%20Shadow.flf > /tmp/figlet/Shadow.flf
+            echo -e "\033[36m$(figlet -f "/tmp/figlet/Shadow.flf" "scala")\033[0m"
+          '';
+        };
+      });
+  };
 }
-
