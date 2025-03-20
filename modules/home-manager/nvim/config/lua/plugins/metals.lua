de841a1ed2bdbc15f81be575fdbf58bb15db8088
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2025-03-20 22:32:50 +0100

    enhance dap

--- modules/home-manager/nvim/config/lua/plugins/metals.lua
+++ modules/home-manager/nvim/config/lua/plugins/metals.lua
@@ -1,226 +1,260 @@
 return {
-  {
-    "scalameta/nvim-metals",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "j-hui/fidget.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    ft = { "scala", "sbt", "java" },
-    opts = function()
-      local metals_config = require("metals").bare_config()
+	{
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"j-hui/fidget.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		ft = { "scala", "sbt", "java" },
+		opts = function()
+			local metals_config = require("metals").bare_config()
 
-      -- Example of settings
-      metals_config.settings = {
-        -- excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-        showImplicitArguments = true,
-        showImplicitConversionsAndClasses = true,
-        showInferredType = true,
-        superMethodLensesEnabled = true,
-        useGlobalExecutable = true,
-        inlayHints = {
-          hintsInPatternMatch = { enable = true },
-          implicitArguments = { enable = true },
-          implicitConversions = { enable = true },
-          inferredTypes = { enable = true },
-          typeParameters = { enable = true },
-        },
-      }
+			-- Example of settings
+			metals_config.settings = {
+				-- excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+				showImplicitArguments = true,
+				showImplicitConversionsAndClasses = true,
+				showInferredType = true,
+				superMethodLensesEnabled = true,
+				useGlobalExecutable = true,
+				inlayHints = {
+					hintsInPatternMatch = { enable = true },
+					implicitArguments = { enable = true },
+					implicitConversions = { enable = true },
+					inferredTypes = { enable = true },
+					typeParameters = { enable = true },
+				},
+			}
 
-      -- *READ THIS*
-      -- I *highly* recommend setting statusBarProvider to either "off" or "on"
-      --
-      -- "off" will enable LSP progress notifications by Metals and you'll need
-      -- to ensure you have a plugin like fidget.nvim installed to handle them.
-      --
-      -- "on" will enable the custom Metals status extension and you *have* to have
-      -- a have settings to capture this in your statusline or else you'll not see
-      -- any messages from metals. There is more info in the help docs about this
-      metals_config.init_options.statusBarProvider = "off"
+			-- *READ THIS*
+			-- I *highly* recommend setting statusBarProvider to either "off" or "on"
+			--
+			-- "off" will enable LSP progress notifications by Metals and you'll need
+			-- to ensure you have a plugin like fidget.nvim installed to handle them.
+			--
+			-- "on" will enable the custom Metals status extension and you *have* to have
+			-- a have settings to capture this in your statusline or else you'll not see
+			-- any messages from metals. There is more info in the help docs about this
+			metals_config.init_options.statusBarProvider = "off"
 
-      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+			metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      metals_config.on_attach = function(client, bufnr)
-        require("metals").setup_dap()
-        -- Define your mappings with descriptions
-        local keymaps = {
-          {
-            "n",
-            "<leader>cW",
-            function()
-              require("metals").hover_worksheet()
-            end,
-            "Metals Worksheet",
-          },
-          {
-            "n",
-            "<leader>cM",
-            function()
-              require("telescope").extensions.metals.commands()
-            end,
-            "Telescope Metals Commands",
-          },
-          -- LSP mappings
-          { "n", "gD",         vim.lsp.buf.declaration,     "Go to declaration" },
-          { "n", "gd",         vim.lsp.buf.definition,      "Go to definition" },
-          { "n", "K",          vim.lsp.buf.hover,           "Hover info" },
-          { "n", "gi",         vim.lsp.buf.implementation,  "Go to implementation" },
-          { "n", "gr",         vim.lsp.buf.references,      "Show references" },
-          { "n", "<leader>D",  vim.lsp.buf.type_definition, "Type definition" },
-          { "n", "<leader>cl", vim.lsp.codelens.run,        "Run CodeLens" },
-          { "n", "<leader>sh", vim.lsp.buf.signature_help,  "Signature help" },
-          { "n", "<leader>rn", vim.lsp.buf.rename,          "Rename symbol" },
-          { "n", "<leader>f",  vim.lsp.buf.format,          "Format code" },
-          { "n", "<leader>ca", vim.lsp.buf.code_action,     "Code action" },
-          -- { "n", "<leader>gds", vim.lsp.buf.document_symbol,                       "Doc symbols" },
-          {
-            "n",
-            "<leader>gds",
-            function()
-              require("telescope.builtin").lsp_document_symbols()
-            end,
-            "Doc symbols",
-          },
-          -- { "n", "gws",         vim.lsp.buf.workspace_symbol,                      "Workspace symbols" },
-          {
-            "n",
-            "<leader>gws",
-            function()
-              require("telescope.builtin").lsp_dynamic_workspace_symbols()
-            end,
-            "Workspace symbols",
-          },
-          {
-            "n",
-            "<leader><leader>tr",
-            function()
-              require("metals.tvp").toggle_tree_view()
-            end,
-            "Tree View Protocol",
-          },
-          {
-            "n",
-            "<leader><leader>rt",
-            function()
-              require("metals.tvp").reveal_in_tree()
-            end,
-            "Reveal In Tree",
-          },
+			metals_config.tvp["icons"] = { enabled = true }
 
-          -- Diagnostic mappings
-          { "n", "<leader>aa", vim.diagnostic.setqflist,  "All diagnostics" },
-          {
-            "n",
-            "<leader>ae",
-            function()
-              vim.diagnostic.setqflist({ severity = "E" })
-            end,
-            "All errors",
-          },
-          {
-            "n",
-            "<leader>aw",
-            function()
-              vim.diagnostic.setqflist({ severity = "W" })
-            end,
-            "All warnings",
-          },
-          { "n", "<leader>d",  vim.diagnostic.setloclist, "Buffer diagnostics" },
-          {
-            "n",
-            "[c",
-            function()
-              vim.diagnostic.goto_prev({ wrap = false })
-            end,
-            "Prev diagnostic",
-          },
-          {
-            "n",
-            "]c",
-            function()
-              vim.diagnostic.goto_next({ wrap = false })
-            end,
-            "Next diagnostic",
-          },
+			metals_config.on_attach = function(client, bufnr)
+				require("metals").setup_dap()
+				-- Define your mappings with descriptions
+				local keymaps = {
+					{
+						"n",
+						"<leader>cW",
+						function()
+							require("metals").hover_worksheet()
+						end,
+						"Metals Worksheet",
+					},
+					{
+						"n",
+						"<leader>cM",
+						function()
+							require("telescope").extensions.metals.commands()
+						end,
+						"Telescope Metals Commands",
+					},
+					-- LSP mappings
+					-- { "n", "gD",         vim.lsp.buf.declaration,     "Go to declaration" },
+					{ "n", "gd", vim.lsp.buf.definition, "Go to definition" },
+					{ "n", "K", vim.lsp.buf.hover, "Hover info" },
+					{ "n", "gi", vim.lsp.buf.implementation, "Go to implementation" },
+					{ "n", "gr", vim.lsp.buf.references, "Show references" },
+					{ "n", "<leader>D", vim.lsp.buf.type_definition, "Type definition" },
+					{ "n", "<leader>cl", vim.lsp.codelens.run, "Run CodeLens" },
+					{ "n", "<leader>sh", vim.lsp.buf.signature_help, "Signature help" },
+					{ "n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol" },
+					{ "n", "<leader>f", vim.lsp.buf.format, "Format code" },
+					{ "n", "<leader>ca", vim.lsp.buf.code_action, "Code action" },
+					-- { "n", "<leader>gds", vim.lsp.buf.document_symbol,                       "Doc symbols" },
+					{
+						"n",
+						"<leader>gds",
+						function()
+							require("telescope.builtin").lsp_document_symbols()
+						end,
+						"Doc symbols",
+					},
+					-- { "n", "gws",         vim.lsp.buf.workspace_symbol,                      "Workspace symbols" },
+					{
+						"n",
+						"<leader>gws",
+						function()
+							require("telescope.builtin").lsp_dynamic_workspace_symbols()
+						end,
+						"Workspace symbols",
+					},
+					{
+						"n",
+						"<leader><leader>tr",
+						function()
+							require("metals.tvp").toggle_tree_view()
+						end,
+						"Tree View Protocol",
+					},
+					{
+						"n",
+						"<leader><leader>rt",
+						function()
+							require("metals.tvp").reveal_in_tree()
+						end,
+						"Reveal In Tree",
+					},
 
-          -- DAP (Debug Adapter Protocol) mappings
-          {
-            "n",
-            "<leader>dc",
-            function()
-              require("dap").continue()
-            end,
-            "DAP continue",
-          },
-          {
-            "n",
-            "<leader>dr",
-            function()
-              require("dap").repl.toggle()
-            end,
-            "DAP REPL",
-          },
-          {
-            "n",
-            "<leader>dK",
-            function()
-              require("dap.ui.widgets").hover()
-            end,
-            "DAP hover",
-          },
-          {
-            "n",
-            "<leader>dt",
-            function()
-              require("dap").toggle_breakpoint()
-            end,
-            "Toggle breakpoint",
-          },
-          {
-            "n",
-            "<leader>dso",
-            function()
-              require("dap").step_over()
-            end,
-            "Step over",
-          },
-          {
-            "n",
-            "<leader>dsi",
-            function()
-              require("dap").step_into()
-            end,
-            "Step into",
-          },
-          {
-            "n",
-            "<leader>dl",
-            function()
-              require("dap").run_last()
-            end,
-            "Run last",
-          },
-        }
+					-- Diagnostic mappings
+					{ "n", "<leader>aa", vim.diagnostic.setqflist, "All diagnostics" },
+					{
+						"n",
+						"<leader>ae",
+						function()
+							vim.diagnostic.setqflist({ severity = "E" })
+						end,
+						"All errors",
+					},
+					{
+						"n",
+						"<leader>aw",
+						function()
+							vim.diagnostic.setqflist({ severity = "W" })
+						end,
+						"All warnings",
+					},
+					{ "n", "<leader>d", vim.diagnostic.setloclist, "Buffer diagnostics" },
+					{
+						"n",
+						"[c",
+						function()
+							vim.diagnostic.goto_prev({ wrap = false })
+						end,
+						"Prev diagnostic",
+					},
+					{
+						"n",
+						"]c",
+						function()
+							vim.diagnostic.goto_next({ wrap = false })
+						end,
+						"Next diagnostic",
+					},
 
-        -- Apply mappings in a loop
-        for _, keymap in ipairs(keymaps) do
-          local mode, lhs, rhs, desc = unpack(keymap)
-          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-        end
-      end
+					-- DAP (Debug Adapter Protocol) mappings
+					{
+						"n",
+						"<leader>dc",
+						function()
+							require("dap").continue()
+						end,
+						"DAP continue",
+					},
+					{
+						"n",
+						"<leader>dr",
+						function()
+							require("dap").repl.toggle()
+						end,
+						"DAP REPL",
+					},
+					{
+						"n",
+						"<leader>dK",
+						function()
+							require("dap.ui.widgets").hover()
+						end,
+						"DAP hover",
+					},
+					{
+						"n",
+						"<leader>dt",
+						function()
+							require("dap").toggle_breakpoint()
+						end,
+						"Toggle breakpoint",
+					},
+					{
+						"n",
+						"<leader>dso",
+						function()
+							require("dap").step_over()
+						end,
+						"Step over",
+					},
+					{
+						"n",
+						"<leader>dsi",
+						function()
+							require("dap").step_into()
+						end,
+						"Step into",
+					},
+					{
+						"n",
+						"<leader>dl",
+						function()
+							require("dap").run_last()
+						end,
+						"Run last",
+					},
+				}
 
-      return metals_config
-    end,
-    config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = self.ft,
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end,
-  },
+				-- Apply mappings in a loop
+				for _, keymap in ipairs(keymaps) do
+					local mode, lhs, rhs, desc = unpack(keymap)
+					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+				end
+
+				local lsp_group = vim.api.nvim_create_augroup("lsp", { clear = true })
+
+				if client.server_capabilities.documentHighlightProvider then
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = bufnr,
+						callback = vim.lsp.buf.document_highlight,
+						group = lsp_group,
+					})
+
+					vim.api.nvim_create_autocmd("CursorMoved", {
+						buffer = bufnr,
+						callback = vim.lsp.buf.clear_references,
+						group = lsp_group,
+					})
+				end
+
+				if client.server_capabilities.codeLensProvider then
+					vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+						buffer = bufnr,
+						callback = vim.lsp.codelens.refresh,
+						group = lsp_group,
+					})
+				end
+
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = { "dap-repl" },
+					callback = function()
+						require("dap.ext.autocompl").attach()
+					end,
+					group = lsp_group,
+				})
+			end
+
+			return metals_config
+		end,
+		config = function(self, metals_config)
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = self.ft,
+				callback = function()
+					require("metals").initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+	},
 }
--- modules/home-manager/nvim/config/lua/plugins/nvim-dap.lua
+++ modules/home-manager/nvim/config/lua/plugins/nvim-dap.lua
@@ -1,27 +1,81 @@
 return {
-  "mfussenegger/nvim-dap",
-  config = function(self, opts)
-    -- Debug settings if you're using nvim-dap
-    local dap = require("dap")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"theHamsta/nvim-dap-virtual-text",
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	ft = { "scala", "sbt", "java" },
+	config = function(_, _)
+		-- Debug settings if you're using nvim-dap
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    dap.configurations.scala = {
-      {
-        type = "scala",
-        request = "launch",
-        name = "RunOrTest",
-        metals = {
-          runType = "runOrTestFile",
-          --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-        },
-      },
-      {
-        type = "scala",
-        request = "launch",
-        name = "Test Target",
-        metals = {
-          runType = "testTarget",
-        },
-      },
-    }
-  end,
+		dap.configurations.scala = {
+			{
+				type = "scala",
+				request = "launch",
+				name = "RunOrTest",
+				metals = {
+					runType = "runOrTestFile",
+					--args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+				},
+			},
+			{
+				type = "scala",
+				request = "launch",
+				name = "Test Target",
+				metals = {
+					runType = "testTarget",
+				},
+			},
+			{
+				name = "Debug Attach (5005)",
+				type = "scala",
+				request = "attach",
+				hostName = "localhost",
+				port = 5005,
+				buildTarget = "root",
+			},
+			{
+				name = "Debug Attach Test (5005)",
+				type = "scala",
+				request = "attach",
+				hostName = "localhost",
+				port = 5005,
+				buildTarget = "root-test",
+			},
+		}
+
+		-- Setup DAPUI
+		dapui.setup({
+			icons = { collapsed = "", current_frame = "", expanded = "" },
+			layouts = {
+				{
+					elements = { "scopes", "watches", "stacks", "breakpoints" },
+					size = 80,
+					position = "left",
+				},
+				{ elements = { "console", "repl" }, size = 0.25, position = "bottom" },
+			},
+			render = { indent = 2 },
+		})
+
+		-- Setup Virtual Text
+		require("nvim-dap-virtual-text").setup({})
+
+		-- Added event for open DAUI
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
 }
