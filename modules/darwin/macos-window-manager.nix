a8be111a9c7c1be0a77595b003481853705381ba
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2025-03-22 20:52:31 +0100

    enhance window manager

--- modules/darwin/macos-window-manager.nix
+++ modules/darwin/macos-window-manager.nix
@@ -1,8 +1,12 @@
-# https://github.com/LnL7/nix-darwin/blob/master/modules/system/defaults/dock.nix
+# https://github.com/LnL7/nix-darwin/blob/master/modules/system/defaults/WindowManager.nix
+# https://www.reddit.com/r/MacOS/comments/15drk66/manipulating_and_managing_windows_without/
 {
-  system.defaults.WindowManager = {
-    EnableTiledWindowMargins = false;
-    EnableTilingByEdgeDrag = true;
-    EnableTopTilingByEdgeDrag = true;
+  system.defaults = {
+    NSGlobalDomain.NSWindowResizeTime = 0.065;
+    WindowManager = {
+      EnableTiledWindowMargins = false;
+      EnableTilingByEdgeDrag = true;
+      EnableTopTilingByEdgeDrag = true;
+    };
   };
 }
