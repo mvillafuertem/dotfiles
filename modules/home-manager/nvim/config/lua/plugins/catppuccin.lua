c31c280efbb051abe6c08192203c900648d462f0
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2025-12-07 20:52:42 +0100

    add catppuccin which_key

--- modules/home-manager/nvim/config/lua/plugins/catppuccin.lua
+++ modules/home-manager/nvim/config/lua/plugins/catppuccin.lua
@@ -4,6 +4,11 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				which_key = true,
+			},
+		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
