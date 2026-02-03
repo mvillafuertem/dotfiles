6647de8468c7571681950b908b921cd81a0672af
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2026-02-03 11:18:04 +0100

    fix treesitter

--- modules/home-manager/nvim/config/lua/plugins/treesitter.lua
+++ modules/home-manager/nvim/config/lua/plugins/treesitter.lua
@@ -1,86 +1,89 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/nvim-treesitter-context"
-  },
-  build = ":TSUpdate",
-  event = "VeryLazy",
-  main = "nvim-treesitter.configs",
-  opts = {
-    -- auto_install = true,
-    ensure_installed = {
-      "bash",
-      "dockerfile",
-      "java",
-      "json",
-      "lua",
-      "markdown",
-      "nix",
-      "rust",
-      "scala",
-      "hcl",
-      "terraform",
-      "toml",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-      -- "gotmpl",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@conditional.outer",
-          ["ic"] = "@conditional.inner",
-          ["al"] = "@loop.outer",
-          ["il"] = "@loop.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]f"] = "@function.outer",
-        },
-        goto_previous_start = {
-          ["[f"] = "@function.outer",
-        },
-      },
-    },
-    ["treesitter-context"] = {
-      enable = true
-    }
-  },
-  -- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-  -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-  -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-  -- keys = {
-  --   {
-  --     ";",
-  --     function()
-  --       require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move_next()
-  --     end,
-  --     mode = { "n", "x", "o" },
-  --     -- silent = true,
-  --     noremap = true,
-  --     desc = "Treesitter Repeat Last Move Next",
-  --   },
-  --   {
-  --     ",",
-  --     function()
-  --       require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move_previous()
-  --     end,
-  --     mode = { "n", "x", "o" },
-  --     -- silent = true,
-  --     noremap = true,
-  --     desc = "Treesitter Repeat Last Move Previous",
-  --   },
-  -- },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"dockerfile",
+				"java",
+				"json",
+				"lua",
+				"markdown",
+				"nix",
+				"rust",
+				"scala",
+				"hcl",
+				"terraform",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@conditional.outer",
+						["ic"] = "@conditional.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+					},
+				},
+			},
+		},
+		config = function()
+			-- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+			-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+			-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+			-- keys = {
+			--   {
+			--     ";",
+			--     function()
+			--       require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move_next()
+			--     end,
+			--     mode = { "n", "x", "o" },
+			--     -- silent = true,
+			--     noremap = true,
+			--     desc = "Treesitter Repeat Last Move Next",
+			--   },
+			--   {
+			--     ",",
+			--     function()
+			--       require("nvim-treesitter.textobjects.repeatable_move").repeat_last_move_previous()
+			--     end,
+			--     mode = { "n", "x", "o" },
+			--     -- silent = true,
+			--     noremap = true,
+			--     desc = "Treesitter Repeat Last Move Previous",
+			--   },
+			-- },
+
+			-- Configure treesitter-context
+			require("treesitter-context").setup({
+				enable = true,
+			})
+		end,
+	},
 }
