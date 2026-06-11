8038cdcd45d28315739a737f47d6d58612ced8e0
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2026-06-11 22:06:35 +0200

    feat(nvim): add Tab/S-Tab keymaps for LuaSnip tabstop navigation

--- modules/home-manager/nvim/config/lua/plugins/luasnip.lua
+++ modules/home-manager/nvim/config/lua/plugins/luasnip.lua
@@ -7,5 +7,12 @@ return {
     luasnip.config.set_config({ history = true, updateevents = "TextChanged,TextChangedI" })
     -- Load custom VSCode-format snippets from ~/.config/nvim/snippets/
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+    -- Jump forward/backward through tabstops
+    vim.keymap.set({ "i", "s" }, "<Tab>", function()
+      if luasnip.jumpable(1) then luasnip.jump(1) end
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+      if luasnip.jumpable(-1) then luasnip.jump(-1) end
+    end, { silent = true })
   end,
 }
